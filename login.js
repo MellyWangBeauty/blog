5d409fc21d255b733eec6c6f30d8399b13e86f2a
MellyWang <2212491506@qq.com>
2022-11-08 23:42:08 +0800

    11.8

--- login.js
+++ login.js
@@ -1,24 +1,18 @@
 const labels = document.querySelectorAll('.form-control label')
-// const btn = document.querySelector('.btn')
 const inputs = document.querySelectorAll('input')
 
-labels.forEach(label => {
+labels.forEach(label=>{
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .map((letter,idx)=>`<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
 })
 
-// btn.addEventListener('click',()=>{
-//     if(inputs[0].value.length!=0 && inputs[1].value.length!=0){
-//         alert('登录成功！')
-//     }
-// })
-
 function jump(){
     if(inputs[0].value.length!=0 && inputs[1].value.length!=0){
-        alert('登录成功！')
+        alert('登录成功！即将跳转回主页')
         window.location.href='./index.html'
         window.event.returnValue=false
     }
 }
+
